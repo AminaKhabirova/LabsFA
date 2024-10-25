8af43b0b266b1f676a0fa24ca2a31dfc14b304de
Amina <khabirovaa10@mail.ru>
2024-10-25 19:18:23 +0300

    Changed: 2.3

--- Laba2/Task3/task3.c
+++ Laba2/Task3/task3.c
@@ -3,78 +3,58 @@
 #include <stdarg.h>
 #include <string.h>
 
-char* my_strstr(const char *haystack, const char *needle, int needle_len) {
-	for (int i = 0; haystack[i] != '\0'; i++) {
-		int j;
-		for (j = 0; j < needle_len && haystack[i + j] == needle[j]; j++);
-		if (j == needle_len) return (char*)(haystack + i);
+char* my_strstr(const char *buffer, const char *substring, size_t substring_len) {
+	for (size_t i = 0; buffer[i] != '\0'; i++) {
+		size_t j;
+		for (j = 0; j < substring_len && buffer[i + j] == substring[j]; j++);
+		if (j == substring_len) return (char*)(buffer + i);
 	}
 	return NULL;
 }
 
-void SymbolsCount(char* input_file) {
-	FILE* input = NULL;
-	char sym;
-	int count = 0;
-	if ((input = fopen(input_file, "r")) == NULL) {
-		printf("Failed to open input file\n");
-		return;
-	}
-	while ((sym = fgetc(input)) != EOF) {
-		if (sym != '\n') {
-			count++;
-		} else {
-			count = 0;
-		}
-	}
-	fclose(input);
-}
-
-void find_substring_in_files(const char *substring, size_t num_files, ...) {
-	if (substring == NULL || num_files == 0) {
+void find_substring_in_files(const char *substring, size_t number_of_files, ...) {
+	if (substring == NULL || number_of_files == 0) {
 		printf("Invalid input data\n");
 		return;
 	}
-	va_list args;
-	va_start(args, num_files);
 
-	int substring_len = 0;
-	int n_count = 0;
+	va_list args;
+	va_start(args, number_of_files);
+
+	// количество символов в подстроке
+	size_t substring_len = 0;
+	size_t n_count = 0;
 	while (substring[substring_len] != '\0') {
 		if (substring[substring_len] == '\n' && substring[substring_len + 1] != '\0') {
 			n_count++;
 		}
 		substring_len++;
 	}
+
 	if (!substring_len) {
 		va_end(args);
 		return;
 	}
 
-	for (int i = 0; i < num_files; i++) {
+	for (int i = 0; i < number_of_files; i++) {
 		const char *filename = va_arg(args, const char*);
 		FILE *file = fopen(filename, "r");
 		if (file == NULL) {
-			perror("Error opening file");
+			printf("Error opening file");
 			continue;
 		}
 
-		int line_number = 1;
-		int line_count = 1;
-		int char_number = 1;
+		// создание буфера
 		char *buffer = malloc((substring_len + 1) * sizeof(char));
 		if (buffer == NULL) {
-			perror("Error allocating memory");
+			printf("Error allocating memory");
 			fclose(file);
 			continue;
 		}
 
-		int buffer_index = 0;
+		// подсчет количества строк в файле
 		char c;
-		int found = 0;
-		size_t count = 0;
-
-
+		size_t line_count = 1;
 		while ((c = fgetc(file)) != EOF) {
 			if (c == '\n') {
 				line_count++;
@@ -82,48 +62,53 @@ void find_substring_in_files(const char *substring, size_t num_files, ...) {
 		}
 		rewind(file);
 
-		size_t *lengths = (size_t *)malloc(line_count * sizeof(size_t));
+		// создание массива с количеством символов в строках
+		size_t *lengths = (size_t*)malloc(line_count * sizeof(size_t));
 		if (lengths == NULL) {
-			perror("Ошибка выделения памяти");
+			printf("Error allocating memory");
 			fclose(file);
 			continue;
 		}
 
+		// подсчет количества символов в текущей строке + символы в предыдущих строках
 		size_t index = 0;
+		size_t size = 0;
 		while ((c = fgetc(file)) != EOF) {
 			if (c != '\n') {
-				count++;
+				size++;
 			} else {
 				if (index == 0) {
-					lengths[index++] = count;
+					lengths[index++] = size;
 				} else {
-					lengths[index++] = lengths[index - 1] + count;
+					lengths[index++] = lengths[index - 1] + size;
 				}
-				count = 0;
+				size = 0;
 			}
 		}
-		if (count) {
+		if (size) {
 			if (index == 0) {
-				lengths[index] = count;
+				lengths[index] = size;
 			} else {
-				lengths[index] = lengths[index - 1] + count;
+				lengths[index] = lengths[index - 1] + size;
 			}
 		}
 		rewind(file);
 
+		size_t line_number = 1;
+		size_t char_number = 1;
+		size_t buffer_index = 0;
 		size_t vsp = 0;
 		while ((c = fgetc(file)) != EOF) {
 			if (c != '\r') buffer[buffer_index++] = c;
 			if (buffer_index == substring_len) {
 				buffer[buffer_index] = '\0';
 				if (my_strstr(buffer, substring, substring_len) != NULL) {
-					found = 1;
 					if (line_number - n_count == 1) {
 						vsp = 0;
 					} else {
 						vsp = lengths[line_number - n_count - 2];
 					}
-					printf("File: %s, Line: %d, Char: %lu\n", filename, line_number - n_count,
+					printf("File: %s, Line: %lu, Char: %lu\n", filename, line_number - n_count,
 						char_number - vsp - substring_len + 1);
 				}
 				for (int j = 0; j < substring_len - 1; j++) {
@@ -135,16 +120,17 @@ void find_substring_in_files(const char *substring, size_t num_files, ...) {
 			if (c == '\n') {
 				line_number++;
 			}
+
 			if (c != '\r') char_number++;
 		}
 
 		// Проверка последнего символа
-		if (found == 0 && buffer_index == substring_len - 1) {
+		if (buffer_index == substring_len - 1) {
 			buffer[buffer_index] = '\0';
 			if (my_strstr(buffer, substring, substring_len) != NULL) {
 				vsp = lengths[line_number - n_count - 2];
 				printf("%lu\n", vsp);
-				printf("File: %s, Line: %d, Char: %lu\n", filename, line_number - n_count,
+				printf("File: %s, Line: %lu, Char: %lu\n", filename, line_number - n_count,
 					char_number - vsp - substring_len + 1);
 			}
 		}
@@ -158,12 +144,12 @@ void find_substring_in_files(const char *substring, size_t num_files, ...) {
 }
 
 int check_string(const char *str) {
-	int length = 0;
+	size_t length = 0;
 	while (str[length] != '\0') {
 		length++;
 	}
 
-	for (int i = length + 1; str[i] != '\0'; i++) {
+	for (size_t i = length + 1; str[i] != '\0'; i++) {
 		if (str[i] != '\0') {
 			return -1;
 		}
@@ -178,6 +164,6 @@ int main() {
 		printf("Invalid input data\n");
 		return 1;
 	}
-	find_substring_in_files(substring, 1, "test.txt");
+	find_substring_in_files(substring, 2, "test.txt", "test3.txt");
 	return 0;
 }
