349d038446c8bc8d813dc9fc89d63df8d2250de6
Amina <khabirovaa10@mail.ru>
2024-10-29 20:21:37 +0300

    Changed: 3.1

--- Laba3/Task1/task1.c
+++ Laba3/Task1/task1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int Add(int a, int b) {
+size_t Add(size_t a, size_t b) {
 	while (b != 0) {
-		int carry = a & b;
+		size_t carry = a & b;
 		a = a ^ b;
 		b = carry << 1;
 	}
@@ -11,9 +11,9 @@ int Add(int a, int b) {
 
 int IsCorrectBase(int base) {
 	for (int i = 1; i <= 5;) {
-		int power_of_two = 1 << i;
+		size_t power_of_two = 1 << i;
 		if (power_of_two == base) return i;
-		i = Add(i, 1);
+		i = (int)Add(i, 1);
 	}
 	return 0;
 }
@@ -23,37 +23,62 @@ int Conversion(int number, int base, char* result) {
 	if (!power_of_two) return -1;
 
 	int negative = number < 0;
+
+	char* res_ptr;
+	size_t index = 0;
+	if (number == 0) {
+		res_ptr = (char*)Add((size_t)result, index);
+		*res_ptr = '0';
+		index = Add(index, 1);
+		res_ptr = (char*)Add((size_t)result, index);
+		*res_ptr = '\0';
+		return 0;
+	}
+
+	size_t new_number;
 	if (negative) {
-		number = Add(~number, 1);
+		number = (int)Add(number, 1);
+		new_number = ~number;
+		new_number = Add(new_number, 2);
+	} else {
+		new_number = number;
 	}
 
 	char vsp[34];
-	int index = 0;
-	while (number > 0) {
-		int digit = number & Add(base, -1);
-		vsp[index] = (digit >= 0 && digit <= 9) ? (char)Add(digit, (int)'0') :
+	char* ptr = vsp;
+	char *new_ptr;
+	while (new_number > 0) {
+		size_t digit = new_number & Add(base, -1);
+		new_ptr = (char*)(Add((size_t)ptr, index));
+		*new_ptr = (digit >= 0 && digit <= 9) ? (char)Add(digit, (int)'0') :
 		(char)Add(Add( digit, (int)'A'), -10);
 		index = Add(index, 1);
-		number >>= power_of_two;
+		new_number >>= power_of_two;
 	}
 
 	if (negative) {
-		vsp[index] = '-';
+		new_ptr = (char*)(Add((size_t)ptr, index));
+		*new_ptr = '-';
 		index = Add(index, 1);
 	}
 
+
 	for (int i = Add(index, -1); i >= 0;) {
-		result[Add(Add(index, -i), -1)] = vsp[i];
+		int res_index = Add(Add(index, Add(~i, 1)), -1);
+		res_ptr = (char*)Add((size_t)result, res_index);
+		new_ptr = (char*)(Add((size_t)ptr, i));
+		*res_ptr = *new_ptr;
 		i = Add(i, -1);
 	}
 
-	result[Add(index, 1)] = '\0';
+	res_ptr = (char*)Add((size_t)result, index);
+	*res_ptr = '\0';
 	return 0;
 }
 
 int main() {
-	int number = 2147483647;
-	int base = 32;
+	int number = 1234;
+	int base = 16;
 	char result[34];
 	if (Conversion(number, base, result)) {
 		printf("Incorrect base\n");
